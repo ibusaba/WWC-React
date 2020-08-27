172b6963574ede98b3c35285c92b692e271a5696
ibusaba <66990249+ibusaba@users.noreply.github.com>
2020-08-26 23:04:43 -0700

    Add files via upload

--- Note.jsx
+++ Note.jsx
@@ -7,7 +7,7 @@ function Notes() {
             
             <p href="">Start here</p>
             <div className="col">
-                    <a role="button" className="btn btn-link" href="www.https://www.sacsheriff.com/Pages/Organization/SIIB/CCW.aspxtel:+12065551234"><i className="fa fa-phone" /> Website</a><br />
+                    <a role="button" className="btn btn-link" href="https://www.sacsheriff.com/Pages/Organization/SIIB/CCW.aspxtel:+12065551234"><i className="fa fa-phone" /> Website</a><br />
                     <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> ccw4women@ccw.co</a>
                 </div>
         </header>
